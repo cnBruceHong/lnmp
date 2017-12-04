798c35380ca9f0b0654bcb4595b550c78e978cce
cnBruceHong <cnbrucehong@icloud.com>
2017-12-04 16:19:00 +0800

    fix

--- startup.sh
+++ startup.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-docker run -d -v $1:/home/web/code -P -p 80:80 --name $2 brucehong/php-dev
+projectName=`pwd | awk -F "/" '{print $NF}'`
+docker run -d -v $PWD:/home/web/code -P -p 80:80 --name $projectName brucehong/php-dev
